803299608893bc6dcc36762bc75d62acf6b3d9c7
Mike Hultquist <mike@Mikes-MacBook-Pro.local>
2012-12-20 15:11:18 -0800

    added rotation

--- ViewController.h
+++ ViewController.h
@@ -10,11 +10,22 @@
 @interface ViewController : UIViewController{
     UIButton *resetGameButton;
     UIButton *setMoveButton;
+    UIButton *rotateCButton;
+    UIButton *switchButton;
+    UIButton *rotateCCButton;
+
+
     NSArray *boardViews;
     NSArray *subBoardArrays;
     
+    float rotations[4];
+    int subIndex;
+    
     CGFloat height;
     CGFloat width;
+    
+    CATransform3D clockwise;
+    CATransform3D counterClock;
 }
 
 @end
--- ViewController.m
+++ ViewController.m
@@ -19,7 +19,6 @@
 }
 - (void)viewDidLoad{
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     [self.view setBackgroundColor:[UIColor blueColor]];
     [self getSize];
     [self setup];
@@ -42,6 +41,7 @@
     int base = [self scale:830];
     int b2 = base/2;
     int side = [self scale:300];
+    subIndex = 0;
     
     UIView *board1 = [[UIView alloc]initWithFrame:CGRectMake(width/2.0-side-1, b2-side-1, side, side)];
     UIView *board2 = [[UIView alloc]initWithFrame:CGRectMake(width/2.0+1, b2-side-1, side, side)];
@@ -49,33 +49,48 @@
     UIView *board4 = [[UIView alloc]initWithFrame:CGRectMake(width/2.0+1, b2+1, side, side)];
     
     boardViews = [[NSArray alloc]initWithObjects:board1,board2,board3,board4, nil];
+    rotations[0] = 0.0f;
+    rotations[1] = 0.0f;
+    rotations[2] = 0.0f;
+    rotations[3] = 0.0f;
 
-    [self setupSubboard:board1];
-    [self setupSubboard:board2];
-    [self setupSubboard:board3];
-    [self setupSubboard:board4];
+    subBoardArrays = [[NSArray alloc]initWithObjects:
+                      [self setupSubboard:board1],
+                      [self setupSubboard:board2],
+                      [self setupSubboard:board3],
+                      [self setupSubboard:board4], nil];
 }
 
 - (int)scale:(int)num{
     return num * height/1004.0;
 }
 
-- (void) setupSubboard:(UIView *)subboard{
+- (NSMutableArray *) setupSubboard:(UIView *)subboard{
     CGFloat size = subboard.bounds.size.width;
     CGFloat imagesize = size/3.0;
-    
+    NSMutableArray *temp = [[NSMutableArray alloc]initWithCapacity:9];
+
     for(int i = 0; i<3; i++){
         for( int j = 0; j<3;j++){
             CGRect imagerect = CGRectMake(imagesize*j, imagesize*i, imagesize-1, imagesize-1);
             UIImageView *place = [[UIImageView alloc]initWithFrame:imagerect];
-            place.image = [UIImage imageNamed:@"sq-bar.jpg"];
+            if(i && j){
+                place.image = [UIImage imageNamed:@"sq-bar.jpg"];
+            }
+            else{
+                place.image = [UIImage imageNamed:@"sq-white.jpg"];
+            }
             place.layer.cornerRadius = imagesize/2.0;
             [place.layer setMasksToBounds:YES];
+            [place.layer setAnchorPoint:CGPointMake(0.5,0.5)];
             [subboard addSubview:place];
+            [temp addObject:place];
         }
     }
     subboard.backgroundColor = [UIColor brownColor];
+    [subboard.layer setAnchorPoint:CGPointMake(0.5,0.5)];
     [self.view addSubview:subboard];
+    return temp;
 }
 
 - (void)createButtons{
@@ -88,6 +103,38 @@
     setMoveButton = [self newButton:width/2.0 + h:y:w:h:
                             @"Set Move":
                             @selector(setMoveSelected:)];
+    rotateCButton = [self newButton:width/2.0 - 50 :0 :50 :50 :@"<<<":@selector(rotateCC:)];
+    switchButton = [self newButton:width/2.0 :0 :50 :50 :@"..." :@selector(subSwitch:)];
+    rotateCCButton = [self newButton:width/2.0 +50 :0 :50 :50 :@">>>":@selector(rotateC:)];
+    
+
+}
+- (IBAction)rotateC:(id)sender{
+    UIView * subboard = [boardViews objectAtIndex:subIndex];
+    NSArray *places = [subBoardArrays objectAtIndex:subIndex];
+    rotations[subIndex] += M_PI/12;
+    [self rotateSubboard:subboard :places :rotations[subIndex]];
+}
+- (IBAction)rotateCC:(id)sender{
+    UIView * subboard = [boardViews objectAtIndex:subIndex];
+    NSArray *places = [subBoardArrays objectAtIndex:subIndex];
+    rotations[subIndex] -= M_PI/12;
+    [self rotateSubboard:subboard :places :rotations[subIndex]];
+}
+- (IBAction)subSwitch:(id)sender{
+    subIndex++;
+    subIndex = subIndex % 4;
+}
+
+- (void)rotateSubboard:(UIView *)subboard:(NSArray *)places:(float)largeRot{
+    clockwise = CATransform3DMakeRotation(largeRot, 0, 0, 1);
+    counterClock = CATransform3DMakeRotation(-largeRot, 0, 0, 1);
+    
+    subboard.layer.transform = clockwise;
+    for(int i = 0;i<[places count];i++){
+        UIImageView *im = [places objectAtIndex:i];
+        im.layer.transform = counterClock;
+    }
 }
 
 //768x1004 iPad dimensions
